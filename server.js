9d5b2790e0b0eaa449490c74c728002c986f2050
nkingdroid <nking@sbsc.com>
2019-10-03 13:02:10 +0100

    checking DB connection

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const errorHandler = require('./helpers/error-handler');
 // We can also use Mongoose to save the data in the database using Mongoose ORM.
 const mongoose = require('mongoose'), 
 config = process.env.MLAB_URL || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/Bdc';
-
+console.log('db config', config)
 
 require('./models/users');
 require('./models/locations');
