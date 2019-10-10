81899c368df53c8e9aa2f9118a437455d05dc332
nkingdroid <nking@sbsc.com>
2019-10-10 09:46:59 +0100

    config file

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const errorHandler = require('./helpers/error-handler');
 // Use mongoose library to set up the database connection with MongoDB.
 // We can also use Mongoose to save the data in the database using Mongoose ORM.
 const mongoose = require('mongoose'),
-  config = process.env.SCALEGRID_URL || process.env.mongolab_url || 'mongodb://localhost:27017/bdc';
+  config = process.env.SCALEGRID_URL || process.env.mongolab_url || 'mongodb://localhost:27017/Bdc';
 
 let options = {
   useNewUrlParser: true
